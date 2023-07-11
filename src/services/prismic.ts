1d9e579da8d66750ee5568ba91f4d6d8f989695f
marcio junior <marcio.junior@bc9.com.br>
2023-07-11 00:34:37 -0300

    de

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,8 +1,8 @@
 import Prismic from 'prismic-javascript';
 import { DefaultClient } from 'prismic-javascript/types/client';
 
-const apiEndpoint = process.env.PRISMIC_API_ENDPOINT!;
-const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
+const apiEndpoint = process.env.NEXT_PUBLIC_PRISMIC_API_ENDPOINT!;
+const accessToken = process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN;
 
 export function getPrismicClient(req?: unknown): DefaultClient {
   const prismic = Prismic.client(apiEndpoint, {
